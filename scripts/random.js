d6ef0d37690882304c648e759143629a8138dc75
guybracha <guy.bracha@gmail.com>
2023-05-01 13:14:19 +0300

    more characters

--- scripts/random.js
+++ scripts/random.js
@@ -1,4 +1,4 @@
-let heroes = ["Paladin", "Voltage", "Astral", "Dragon Fighter", "Size", "Mariposa", "Gigantic", "Comrade", "Tsunami"];
+let heroes = ["Paladin", "Voltage", "Astral", "Dragon Fighter", "Size", "Mariposa", "Gigantic", "Comrade", "Tsunami", "Symbol", "Enforcer", "Northlight", "Melody", "Arc", "Rocket Prime"];
 
 const surprise = heroes[(Math.floor(Math.random() * (heroes.length)))];
 function clickMe(){
