6c2cef1c41e3448f442c808fa57a58eee179634b
guybracha <guy.bracha@gmail.com>
2023-05-18 11:57:28 +0300

    more js

---
+++ assets/scripts/list.js
@@ -0,0 +1,17 @@
+let list=[
+    {Name: "פלאדין"},
+    {Name: "וולטאז'"},
+    {Name: "אסטרל"},
+    {Name: "דרגון פייטר"},
+    {Name: "סייז"},
+    {Name: "מריפוסה"},
+    {Name: "קומראד"},
+    {Name: "ג'ייגנטיק"},
+    {Name: "צונאמי"},
+    {Name: "סימבול"}
+];
+
+var i;
+for(i = 0; i < list.length; i++){
+    console.log("גיבור מס'" + i + " " + list[i].Name);
+}
--- assets/scripts/random.js
+++ assets/scripts/random.js
@@ -1,6 +1,7 @@
-let heroes = ["Paladin", "Voltage", "Astral", "Dragon Fighter", "Size", "Mariposa", "Gigantic", "Comrade", "Tsunami", "Symbol", "Enforcer", "Northlight", "Melody", "Techno", "Rocket Prime"];
+let heroes = ["פלאדין", "וולטאז'", "אסטרל", "דרגון פייטר", "סייז", "מריפוסה", "ג'ייגנטיק", "קומראד", "צונאמי", "סימבול", "אנפורסר", "נורת'לייט", "מלודי", "טכנו", "רוקט פריים"];
 
 const surprise = heroes[(Math.floor(Math.random() * (heroes.length)))];
 function clickMe(){
-alert("This time the hero is " + surprise);
+    document.getElementById("random").innerHTML = "<h1>והגיבור שלנו הוא: " + surprise + "</h1>";
+    document.getElementById("random").style = "font-size: 40px; color: blue; text-align: center;";
 }
