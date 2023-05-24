6448dd4a2f3c5f9ee14e3e46e6969bb773a66f90
guybracha <guy.bracha@gmail.com>
2023-05-24 11:55:03 +0300

    changing

--- assets/scripts/taleone.js
+++ assets/scripts/taleone.js
@@ -1,32 +1,48 @@
 function loadChapter(chapterNumber) {
-    fetch('chapter' + chapterNumber + '.txt')
-      .then(response => response.text())
-      .then(text => {
-        document.getElementById("storyone").innerHTML = text;
-        processChapter(chapterNumber);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
+  let text;
+
+  switch (chapterNumber) {
+    case 1:
+      // Chapter 1 text
+      text = `
+        <p>This is the first chapter of the story.</p>
+        <p>Here is some more text.</p>
+      `;
+      break;
+    case 2:
+      // Chapter 2 text
+      text = `
+        <h1>Chapter 2</h1>
+        <p>This is the second chapter.</p>
+        <p>More text goes here.</p>
+      `;
+      break;
+    // Add more cases for additional chapters
+    default:
+      // Handle the end of the story or unrecognized chapter
+      text = "<p>End of the story.</p>";
   }
-  
-  function processChapter(chapterNumber) {
-    // Perform additional processing based on the chapter
-    switch (chapterNumber) {
-      case 1:
-        // Set up choices for Chapter 1
-        document.getElementById("storyone").innerHTML += '<button onclick="loadChapter(2)">Go left</button>';
-        document.getElementById("storyone").innerHTML += '<button onclick="loadChapter(3)">Go right</button>';
-        break;
-      case 2:
-        // Set up choices for Chapter 2
-        document.getElementById("storyone").innerHTML += '<button onclick="loadChapter(4)">Enter the cave</button>';
-        document.getElementById("storyone").innerHTML += '<button onclick="loadChapter(5)">Continue along the path</button>';
-        break;
-      // Add more cases for additional chapters
-      default:
-        // Handle the end of the story or unrecognized chapter
-        document.getElementById("storyone").innerHTML += "<p>End of the story.</p>";
-    }
+
+  document.getElementById("storyone").innerHTML = text;
+  processChapter(chapterNumber);
+}
+
+function processChapter(chapterNumber) {
+  // Perform additional processing based on the chapter
+  switch (chapterNumber) {
+    case 1:
+      // Set up choices for Chapter 1
+      document.getElementById("storyone").innerHTML += '<button onclick="loadChapter(2)">Go left</button>';
+      document.getElementById("storyone").innerHTML += '<button onclick="loadChapter(3)">Go right</button>';
+      break;
+    case 2:
+      // Set up choices for Chapter 2
+      document.getElementById("storyone").innerHTML += '<button onclick="loadChapter(4)">Enter the cave</button>';
+      document.getElementById("storyone").innerHTML += '<button onclick="loadChapter(5)">Continue along the path</button>';
+      break;
+    // Add more cases for additional chapters
+    default:
+      // Handle the end of the story or unrecognized chapter
+      document.getElementById("storyone").innerHTML += "<p>End of the story.</p>";
   }
-  
+}
