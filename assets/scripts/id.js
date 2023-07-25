d4cdff72adfa3b14522fa653cb59e9793da978e4
guybracha <guy.bracha@gmail.com>
2023-07-25 13:37:11 +0300

    new objects

--- assets/scripts/id.js
+++ assets/scripts/id.js
@@ -18,6 +18,16 @@ let Hero = [{
     Private_Name: "מיכה הייד",
     Powers: "שרירי על וחוזקה אדירה",
     Image: "contents\\avatars\\gigantic.png",
+}, {
+    Hero_Name: "קומראד",
+    Private_Name: "מיכה אלחימסקי",
+    Powers: "ספיגה גרעינית וירייתה החוצה",
+    Image: "contents\\avatars\\comrade.png",
+},{
+    Hero_Name: "מריפוסה",
+    Private_Name: "נלי אנדור",
+    Powers: "קסם פיות",
+    Image: "contents\\avatars\\mariposa.png",
 }];
 
 const Random = Hero[Math.floor(Math.random() * Hero.length)];
