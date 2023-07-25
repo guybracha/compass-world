2513dbccca5980d1e2b482afe97767a1c455c3eb
guybracha <guy.bracha@gmail.com>
2023-07-25 14:00:40 +0300

    more characters

--- assets/scripts/id.js
+++ assets/scripts/id.js
@@ -28,6 +28,26 @@ let Hero = [{
     Private_Name: "נלי אנדור",
     Powers: "קסם פיות",
     Image: "contents\\avatars\\mariposa.png",
+},{
+    Hero_Name: "סימבול",
+    Private_Name: "נתניאל כהן",
+    Powers: "יכולות על אנושיות",
+    Image: "contents\\avatars\\symbol.png",
+},{
+    Hero_Name: "צונאמי",
+    Private_Name: "טליה",
+    Powers: "שליטה במים",
+    Image: "contents\\avatars\\tsunami.png",
+},{
+    Hero_Name: "דרגון פייטר",
+    Private_Name: "בלייק וו-צ'אנג",
+    Powers: "שליטת בסודות הקונג-פו",
+    Image: "contents\\avatars\\dragon.png",
+},{
+    Hero_Name: "סייז",
+    Private_Name: "בן גורדון",
+    Powers: "גדילה וצמיחה לממדי על",
+    Image: "contents\\avatars\\size.png",
 }];
 
 const Random = Hero[Math.floor(Math.random() * Hero.length)];
