a51fc8c215d4ebc80b6a785bc5fd31b84d2ef170
guybracha <guy.bracha@gmail.com>
2023-07-25 12:31:38 +0300

    more objects

--- assets/scripts/id.js
+++ assets/scripts/id.js
@@ -2,17 +2,22 @@ let Hero = [{
     Hero_Name: "פלאדין",
     Private_Name: "מרדכי רמון",
     Powers: "רפלקסים משופרים",
-    Image: "C:\\Users\\guybr\\OneDrive\\שולחן העבודה\\CompassHtml\\compass-world\\assets\\scripts\\ref\\paladin.png",
+    Image: "contents\\avatars\\paladin.png",
 }, {
     Hero_Name: "אסטרל",
     Private_Name: "מיאנסקה",
     Powers: "טלפתיה וראיית העתיד",
-    Image: "C:\\Users\\guybr\\OneDrive\\שולחן העבודה\\CompassHtml\\compass-world\\assets\\scripts\\ref\\astral.png",
+    Image: "contents\\avatars\\astral.png",
 }, {
     Hero_Name: "וולטאז'",
     Private_Name: "אלק אטלס",
     Powers: "יריית חשמל ומהירות על",
-    Image: "C:\\Users\\guybr\\OneDrive\\שולחן העבודה\\CompassHtml\\compass-world\\assets\\scripts\\ref\\voltage.png",
+    Image: "contents\\avatars\\voltage.png",
+}, {
+    Hero_Name: "ג'ייגנטיק",
+    Private_Name: "מיכה הייד",
+    Powers: "שרירי על וחוזקה אדירה",
+    Image: "contents\\avatars\\gigantic.png",
 }];
 
 const Random = Hero[Math.floor(Math.random() * Hero.length)];
