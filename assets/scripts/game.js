8501a67136813284485d9b6fc2c2270c5b8d5d04
guybracha <guy.bracha@gmail.com>
2024-04-01 10:39:39 +0300

    cleaning scripts

--- assets/scripts/game.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector('#choose');
-        const sb = document.querySelector('#framework')
-        btn.onclick = (event) => {
-            event.preventDefault();
-            // show the selected index
-            alert(sb.selectedIndex);
-        };
--- assets/scripts/read.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('infl').addEventListener('change',function(){
-    var file_reader=new FileReader();
-    file_reader.onload=function(){
-        document.getElementById('res').textContent=file_reader.result;
-    }
-    file_reader.readAsText(this.files[0]);
-})
