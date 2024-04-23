b98219ff936d26956e91f4c1ea7f740024be5786
guybracha <guy.bracha@gmail.com>
2024-04-23 11:46:24 +0300

    starting redesign in passover

--- assets/scripts/random.js
+++ assets/scripts/random.js
@@ -2,6 +2,6 @@ let heroes = ["פלאדין", "וולטאז'", "אסטרל", "דרגון פיי�
 
 const surprise = heroes[(Math.floor(Math.random() * (heroes.length)))];
 function clickMe(){
-    document.getElementById("random").innerHTML = "<h1>והגיבור שלנו הוא: " + "<i>" + surprise + "</i>" + "</h1>";
+    document.getElementById("random").innerHTML = "<h1>And our Hero is... " + "<i>" + surprise + "</i>" + "</h1>";
     document.getElementById("random").style = "font-size: 40px; color: blue; text-align: center;";
 }
