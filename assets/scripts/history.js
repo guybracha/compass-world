facb29c72cca92e4f52a39d94f464cd286ee3f07
guybracha <guy.bracha@gmail.com>
2024-08-20 13:00:35 +0300

    redesigning the site

--- assets/scripts/history.js
+++ assets/scripts/history.js
@@ -1,12 +1,12 @@
-const init = () =>{
+const init = () => {
     createList();
 }
 
 const createList = () => {
-    HistoryList.forEach((item) =>{
-        let vip1 = new HistoryClass(item.year,item.description);
-        vip1.render();
-    })
+    HistoryList.forEach((item, index) => {
+        let historyItem = new HistoryClass(item.year, item.description, index);
+        historyItem.render();
+    });
 }
 
 init();
--- src/main.ts
+++
@@ -1,1 +0,0 @@
-console.log("created and developed by Guy Bracha");
