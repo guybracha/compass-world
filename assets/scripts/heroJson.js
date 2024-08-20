c57fce0bb9972b99e0420db42184b6823d606a03
guybracha <guy.bracha@gmail.com>
2024-08-20 13:06:52 +0300

    changing json

--- assets/scripts/heroJson.js
+++ assets/scripts/heroJson.js
@@ -2,109 +2,121 @@ const superList =  [{
     "superName": "Paladin",
     "privateName": "Yoav Ramon",
     "Powers": "High Reflexes skill",
-    "img": "contents\\avatars\\paladin.png",
+    "img": "contents\\avatar2024\\heroes\\paladin.png",
     "quote": "Alliance, to Action"
     },
     {
     "superName": "Astral",
     "privateName": "Princess Mianeska",
     "Powers": "Cosmic Strength and mist generation",
-    "img": "contents\\avatars\\astral.png",
+    "img": "contents\\avatar2024\\heroes\\astral.png",
     "quote": "ברית, לפעולה!"
     },{
     "superName": "Voltage",
     "privateName": "Alec Atlas",
     "Powers": "Electricity",
-    "img": "contents\\avatars\\voltage.png",
+    "img": "contents\\avatar2024\\heroes\\voltage.png",
     "quote": "ברית, לפעולה!"
     },{
     "superName": "Gigantic",
     "privateName": "Michael Hyde",
     "Powers": "Big Muscles",
-    "img": "contents\\avatars\\gigantic.png",
+    "img": "contents\\avatar2024\\heroes\\gigantic.png",
     "quote": "ברית, לפעולה!"
     },{
     "superName":"Comrade",
     "privateName": "Ruslan Choeminsky",
     "Powers": "Nuclear Blasts",
-    "img": "contents\\avatars\\comrade.png",
+    "img": "contents\\avatar2024\\heroes\\comrade.png",
     "quote": "ברית, לפעולה!"
     },{
     "superName":"Tsunami",
     "privateName": "Talia",
     "Powers": "Hydrokinesis",
-    "img": "contents\\avatars\\tsunami.png",
+    "img": "contents\\avatar2024\\heroes\\tsunami.png",
     "quote": "ברית, לפעולה!"
     },{
     "superName":"Dragon Fighter",
     "privateName": "Blake Wu-Chang",
     "Powers": "Martial Art Master",
-    "img": "contents\\avatars\\dragon.png",
+    "img": "contents\\avatar2024\\heroes\\dragonFighter.png",
     "quote": "ברית, לפעולה!"
     },{
         "superName":"Seraph",
         "privateName": "Nate",
         "Powers": "Synthetic Man",
-        "img": "contents\\avatars\\symbol.png",
+        "img": "contents\\avatar2024\\heroes\\seraph.png",
         "quote": "ברית, לפעולה!"
     },{
         "superName":"Size",
         "privateName": "Ben Gordon",
         "Powers": "Size Changing",
-        "img": "contents\\avatars\\size.png",
+        "img": "contents\\avatar2024\\heroes\\size.png",
         "quote": "ברית, לפעולה!"
     },{
         "superName":"Mariposa",
         "privateName": "Nelly Andor",
         "Powers": "Magic",
-        "img": "contents\\avatars\\mariposa.png",
+        "img": "contents\\avatar2024\\heroes\\mariposa.png",
         "quote": "ברית, לפעולה!"
     },{
         "superName":"Enfocer",
         "privateName": "Eli Ramon",
         "Powers": "Super-Detective",
-        "img": "contents\\avatars\\enforcer.png",
+        "img": "contents\\avatar2024\\heroes\\enforcer.png",
         "quote": "ברית, לפעולה!"
     },{
         "superName":"Melody",
         "privateName": "Shira Ramon",
         "Powers": "Super Singing",
-        "img": "contents\\avatars\\melody.png",
+        "img": "contents\\avatar2024\\heroes\\melody.png",
         "quote": "ברית, לפעולה!"
     },{
         "superName":"Techno",
-        "privateName": "אריק קרלטון",
+        "privateName": "אליס קרלטון",
         "Powers": "טכנופתיה - תקשורת עם מכונות",
-        "img": "contents\\avatars\\techno.png",
+        "img": "contents\\avatar2024\\heroes\\techno.png",
         "quote": "ברית, לפעולה!"
     },{
         "superName":"Rocket Prime",
         "privateName": "חי גרינברג",
         "Powers": "שריון טילים היכול להטיס אותו",
-        "img": "contents\\avatars\\rocket.png",
+        "img": "contents\\avatar2024\\heroes\\rocketPrime.png",
         "quote": "ברית, לפעולה!"
     },{
         "superName":"Northlight",
         "privateName": "אור אנרדסן",
         "Powers": "יצירת אורות",
-        "img": "contents\\avatars\\northlight.png",
+        "img": "contents\\avatar2024\\heroes\\northlight.png",
         "quote": "ברית, לפעולה!"
     },{
         "superName":"Captain Space",
         "privateName": "דניאל אוקס",
         "Powers": "יצירת אובייקטים מריק",
-        "img": "contents\\avatars\\captain.png",
+        "img": "contents\\avatar2024\\heroes\\savior.png",
         "quote": "ברית, לפעולה!"
     },{
-        "superName":"Harkadius",
+        "superName":"East",
         "privateName": "הרקאדיוס",
         "Powers": "קנטאור, מומחה בחרבות",
-        "img": "contents\\avatars\\harkadius.png",
+        "img": "contents\\avatar2024\\heroes\\east.png",
         "quote": "ברית, לפעולה!"
     },{
-        "superName":"Hallethea",
+        "superName":"Sun",
         "privateName": "הייליתיאה",
         "Powers": "קנטאורית, מומחית בחץ וקשת",
-        "img": "contents\\avatars\\hallethea.png",
+        "img": "contents\\avatar2024\\heroes\\sun.png",
+        "quote": "ברית, לפעולה!"
+    },{
+        "superName":"Zero",
+        "privateName": "הייליתיאה",
+        "Powers": "קנטאורית, מומחית בחץ וקשת",
+        "img": "contents\\avatar2024\\heroes\\zero.png",
+        "quote": "ברית, לפעולה!" 
+    },{
+        "superName":"Lion-Man",
+        "privateName": "הייליתיאה",
+        "Powers": "קנטאורית, מומחית בחץ וקשת",
+        "img": "contents\\avatar2024\\heroes\\lionMan.png",
         "quote": "ברית, לפעולה!"
     }];
