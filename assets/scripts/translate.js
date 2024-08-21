1d653c7a63e5ed872721aa690add78ac67051e0d
guybracha <guy.bracha@gmail.com>
2024-08-21 15:42:28 +0300

    about me

--- assets/scripts/translate.js
+++ assets/scripts/translate.js
@@ -1,3 +1,7 @@
 function googleTranslateElementInit() {
-    new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+    new google.translate.TranslateElement({
+        pageLanguage: 'en',
+        includedLanguages: 'es,fr,de,ru,ar', // Add the languages you want to support
+        layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+    }, 'google_translate_element');
 }
