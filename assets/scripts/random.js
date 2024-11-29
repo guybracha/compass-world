06dfb53bb7d97cb9dd003711fe00294d2cbe59fa
guybracha <guy.bracha@gmail.com>
2024-11-29 10:14:01 +0200

    redesign the alliance

--- assets/scripts/random.js
+++ assets/scripts/random.js
@@ -1,4 +1,4 @@
-let heroes = ["Paladin","Voltage","Astral","Gigatnic","Comrade","Tsunami","East","Sun","Jupiterian"];
+let heroes = ["Paladin","Voltage","Astral","Gigatnic","Comrade","Tsunami","Eastwind","Sunshine","Jupiterian"];
 
 const surprise = heroes[(Math.floor(Math.random() * (heroes.length)))];
 function clickMe(){
