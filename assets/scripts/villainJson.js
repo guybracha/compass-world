b99da8b768bb1ad8cdde171c16aab81ddcfe9e93
guybracha <guy.bracha@gmail.com>
2025-08-31 15:27:09 +0300

    webp

--- assets/scripts/villainJson.js
+++ assets/scripts/villainJson.js
@@ -1,13 +1,13 @@
 const villainList = [{
     "supername": "Doctor Monstro",
     "powers": "Mastermind of science schemes",
-    "img": "contents\\avatar2024\\villains\\DrMonstro-min.png",
+    "img": "contents\\avatar2024\\villains\\DrMonstro.webp",
 },{
     "supername": "Headmaster Vile",
     "powers": "Evil Principal of the malice school for young supervillains",
-    "img": "contents\\avatar2024\\villains\\headmasterVile-min.png",
+    "img": "contents\\avatar2024\\villains\\headmasterVile.webp",
 },{
     "supername": "Hypno",
     "powers": "Can hypnotise everyone",
-    "img": "contents\\avatar2024\\villains\\hypno-min.png",
+    "img": "contents\\avatar2024\\villains\\hypno.webp",
 }];
